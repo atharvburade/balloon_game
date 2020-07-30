1c11bb6db58ae54d60436e729b09b14166f955f6
Atharv Burade <atharvburade@gmail.com>
2020-07-30 04:40:04 -0700

    Add unity project

---
+++ Assets/scripts/ballonscript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class ballonscript : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * Time.deltaTime * 0.2f);
+    }
+}
