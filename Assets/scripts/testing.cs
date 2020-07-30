938da7c42cf1160351d4cda3916c77dbec6f4649
Atharv Burade <atharvburade@gmail.com>
2020-07-30 11:17:22 -0700

    Edited the script

--- Assets/scripts/testing.cs
+++ Assets/scripts/testing.cs
@@ -7,19 +7,4 @@ public class SpawnScript : MonoBehaviour
     public Transform[] spawnPoints;
     public GameObject[] balloons;
     // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(StartSpawning());
     }
-
-    IEnumerator StartSpawning()
-    {
-        yield return new WaitForSeconds(4);
-
-        for (int i =0; i < 3; i++)
-        {
-            Instantiate(balloons[i], spawnPoints[i].position, Quaternion.identity);
-        }
-        StartCoroutine(StartSpawning());
-    }
-}
